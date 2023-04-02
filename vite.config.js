2f6cd366d5169f29a2eda2182b9aa3439e40e9a9
Bezirgen <anonimanonimus955@gmail.com>
2023-04-02 13:03:43 +0500

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				students3222: resolve(__dirname, 'articles/students-3222.html'),
+
+
+				bezirgenYaylymow: resolve(__dirname, 'articles/3222/bezirgen-yaylymow.html'),
+			},
+		},
+	},
+})
